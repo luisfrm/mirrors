a4822cd4a629c467899ef6705ec65878c7c77b92
manuelperche <manuelperche@gmail.com>
2022-01-29 19:05:41 -0400

    changed layout

--- src/Characters/character.gd
+++ src/Characters/character.gd
@@ -7,27 +7,18 @@ var coins = 0
 export(float) var flip = 1.0
 
 export(float) var time_to_peak = 0.6 # seconds
-export(float) var jump_height = 64.0 # pixels
+export(float) var jump_height = 100.0 # pixels
 
 onready var gravity = flip * (2.0*jump_height / pow(time_to_peak, 2.0))
 onready var jump_speed = -gravity * time_to_peak 
 onready var vectorFloor = Vector2.DOWN * Vector2(1.0, flip)
 onready var vectorJump = Vector2.UP * Vector2(1.0, flip)
 
-onready var floor_checker = $Check
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	print("gravity ", gravity)
-	print("Jump ", jump_speed)
+	pass
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_up") and (is_on_floor()):
+	if Input.is_action_just_pressed("ui_select") and (is_on_floor()):
 		velocity.y = jump_speed
 	if Input.is_action_pressed("ui_right"):
 		_animated_sprite.animation = "run"

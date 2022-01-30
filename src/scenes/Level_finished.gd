a976e64257195c1805cbdfb251727d28b960a763
Luis <luisfrm_@outlook.com>
2022-01-30 19:06:10 -0400

    little fixes when press enter in homepage

--- src/scenes/Level_finished.gd
+++ src/scenes/Level_finished.gd
@@ -5,6 +5,5 @@ export var levelName = "Level_2.tscn"
 func _ready():
 	pass
 
-
 func _on_changeSceneButton_pressed():
 	get_tree().change_scene("res://src/scenes/" + levelName)
--- src/scenes/MainMenu.gd
+++ src/scenes/MainMenu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _ready():
 	MusicController.play_music()
+
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		get_tree().change_scene("res://src/scenes/Level_1.tscn")

cddff7b60e6834388aa4abfaba29290e8fe22a0a
Luis <luisfrm_@outlook.com>
2022-01-30 04:58:06 -0400

    started second level and enemy2

--- src/Enemies/Bullet.gd
+++ src/Enemies/Bullet.gd
@@ -3,10 +3,10 @@ extends Area2D
 var move = Vector2.ZERO
 var look_vec = Vector2.ZERO
 var player = null
-var speed = 1
+var speed = 3
 
 func _ready():
-	look_vec = player.position - global_position
+	look_vec = player.position - get_global_position()
 
 func _physics_process(delta):
 	move = Vector2.ZERO
@@ -14,3 +14,13 @@ func _physics_process(delta):
 	move = move.move_toward(look_vec, delta)
 	move = move.normalized() * speed
 	position += move
+
+
+func _on_Area2D_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_attack_body_entered(body):
+	if (body == get_parent().get_node("character")):
+		body.die()
+	

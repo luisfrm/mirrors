497dafb1c1f74b405ba1acc7cc71f6bd75ef577d
Luis <luisfrm_@outlook.com>
2022-01-30 15:47:21 -0400

    level 2 adjustenments and gameover added

--- HUD.gd
+++ HUD.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 var coins = 0
 export var coins_to_win = 10
+export var current_level = 1
 onready var Coins = $Coins
 
 onready var LEVEL_FINISHED = preload("res://src/scenes/Level_finished.tscn")
@@ -23,8 +24,19 @@ func _on_coin_collected():
 			get_parent().get_node("Enemy1").isQuiet = true
 		if (get_parent().get_node("Enemy2")):
 			get_parent().get_node("Enemy2").isQuiet = true
+		if (get_parent().get_node("Enemy3")):
+			get_parent().get_node("Enemy3").isQuiet = true
+		if (get_parent().get_node("Enemy4")):
+			get_parent().get_node("Enemy4").isQuiet = true
+		if (get_parent().get_node("Enemy5")):
+			get_parent().get_node("Enemy5").isQuiet = true
+		if (get_parent().get_node("Enemy6")):
+			get_parent().get_node("Enemy6").isQuiet = true
 		var levelFinished = LEVEL_FINISHED.instance()
-		levelFinished.levelName = "Level_2.tscn"
+		if current_level == 2:
+			levelFinished.levelName = "MainMenu.tscn"
+			levelFinished.get_node("Label").text = "Congrats! You've completed the game."
+			levelFinished.get_node("changeSceneButton").text = "Go to Homepage"
 		get_parent().add_child(levelFinished)
 
 func update_coins():
--- src/scenes/Game_Over.gd
+++ src/scenes/Game_Over.gd
@@ -5,3 +5,7 @@ func _ready():
 
 func _on_tryAgain_Button_pressed():
 	get_tree().reload_current_scene()
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().reload_current_scene()
